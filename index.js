2bf0a8401ff47f67d14aeed0477b6f69339f4efa
Dennis <dennis.brian.pv@gmail.com>
2018-04-22 22:14:34 -0400

    Creando el servidor con node y express

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 'use strict' //nos permite acceder a los estandares del lenguaje y sea compatible en navegadores
 
 var mongoose = require('mongoose');
+var app = require('./app');
+var port = process.env.PORT || 3789;
 
 mongoose.connect('mongodb://localhost:27017/ngZoo', (err, res)=>{
     if(err){
         throw err;
     }else
         console.log('La conexion a BD exitosa');
+        app.listen(port, ()=> {
+            console.log("El servidor local con node y express esta corriendo correctamente");
+        });
 })
