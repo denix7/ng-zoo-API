7a2e03c05886d4ffbc3245cf84256064cf032cc5
Dennis <dennis.brian.pv@gmail.com>
2018-04-24 16:11:08 -0400

    Creando el modelo de user y animal con mongoose

---
+++ models/user.js
@@ -0,0 +1,15 @@
+//representa el esquema que tendra cada documento
+'use strict'//para tener las versiones mas modernas de js
+
+var mongoose = require('mogoose');
+var Schema = mongoose.Schema;
+
+var UserSchema = Schema({//datos que representan una entidad de la BD
+    name: String,
+    surname: String,
+    email: String,
+    password: String,
+    rol: String
+});
+
+module.exports = mongoose.model('User', UserSchema)//exportamos entidad que representa a user
