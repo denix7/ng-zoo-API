e3d173433536e87d36a9424ae667cfa3e54c4e16
Dennis <dennis.brian.pv@gmail.com>
2018-04-25 12:07:57 -0400

    Verificar usuarios duplicados en BD con findOne

--- controllers/user.js
+++ controllers/user.js
@@ -19,7 +19,7 @@ function saveUser(req, res){
     //Recojer parametros de la peticion con body parser y convierte a json
     var params = req.body;
     
-    if(params.password && params.name && params.email){
+    if(params.password && params.name && params.email){//parametros que se deben enviar obligatoriamente
         
         //Asignar valores recogidos al objeto de usuario
         user.name = params.name;
@@ -28,23 +28,35 @@ function saveUser(req, res){
         user.role  = 'ROLE_USER';
         user.image = null;
 
-        //cifrar contraseña
-        bcrypt.hash(params.password, null, null, function(err, hash){
-            user.password = hash;
+        User.findOne({email: user.email.toLowerCase()}, (err, issetUser)=>{//busca en BD que el email no exista y evitar usuarios duplicados
+            if(err)
+                res.status(500).send({message: 'Error al comprobar que el usuario existe'});
+            else    
+                {
+                   if(!issetUser){//sino existe el usuario en BD, insertamos el nuevo 
+                        //cifrar contraseña
+                        bcrypt.hash(params.password, null, null, function(err, hash){//ciframos la contraseña
+                        user.password = hash;
 
-            //guardo usuario en BD
-            user.save((err, userStored)=>{
-                if(err)
-                    res.status(500).send({message: 'Error al guardar el usuario'});
-                else{
-                    if(!userStored)
-                        res.status(404).send({message: 'No se ha registrado el usuario'});
-                    else    
-                        res.status(200).send({user: userStored});    
+                            //guardo usuario en BD
+                            user.save((err, userStored)=>{
+                                if(err)
+                                    res.status(500).send({message: 'Error al guardar el usuario'});
+                                else{
+                                    if(!userStored)//sino existe user correcto
+                                        res.status(404).send({message: 'No se ha registrado el usuario'});
+                                    else    
+                                        res.status(200).send({user: userStored});//si hay user correcto guardamos el user en BD    
+                                }    
+
+                            });
+                        });
+                   }else{
+                        res.status(200).send({message: 'El usuario ya existe no puede insertarse'});
+                   } 
                 }    
-
-            });
-        });
+        })    
+            
     }else{
         res.status(200).send({message: 'Introduce los datos correctamente para registrar al usuario'});
     }
