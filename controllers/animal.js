571a9527bec7440195d9de7d9f2ecf02bb6a7c57
Dennis <dennis.brian.pv@gmail.com>
2018-04-29 13:14:31 -0400

    Crenado metodo para registrar animales

--- controllers/animal.js
+++ controllers/animal.js
@@ -15,6 +15,38 @@ function pruebas(req, res){
     });
 }
 
+function saveAnimal(req, res){
+    //crear objeto animal
+    var animal = Animal();
+
+    //Recojer peticion post por body parser
+    var params = req.body;
+
+    if(params.name && params.description)
+    {
+        animal.name = params.name;
+        animal.description = params.description;
+        animal.year = params.year;
+        animal.image = null;
+        animal.user = req.user.sub;//guarda id del user logueado
+
+        animal.save((err, animalStored)=>{
+            if(err)
+                res.status(500).send({message: 'Error al guardar el animal'});
+            else{
+                if(!animalStored)//sino llega animal correcto
+                    res.status(404).send({message: 'No se ha registrado el animal'});
+                else    
+                    res.status(200).send({animal: animalStored});//si hay animal correcto guardamos el user en BD    
+            }    
+        })
+    
+    }else{
+        res.status(500).send({message: 'Nombre y description de animal es obligatorio'});
+    }
+}
+
 module.exports = {
-    pruebas
+    pruebas,
+    saveAnimal
 }
--- routes/animal.js
+++ routes/animal.js
@@ -10,5 +10,6 @@ var multipart = require('connect-multiparty');//permite manipular files
 var md_upload = multipart({uploadDir: '../uploads/animals'});//creamos el folder donde se alojaran ficheros de animals 
 
 api.get('/animal', md_auth.ensureAuth, animalController.pruebas);
+api.post('/save-animal', md_auth.ensureAuth, animalController.saveAnimal);
 
 module.exports = api;
